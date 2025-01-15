4eb0688961d5f980fe583aba73864fc0e23bdc37
TanvirSajib <tanvir.sajib.programmer@gmail.com>
2025-01-15 16:34:18 +0600

    sarcasticCase2 function according to tutorial

--- main.js
+++ main.js
@@ -25,3 +25,22 @@ function sarcasticCase(str){
 }
 
 console.log(sarcasticCase('Hello There eroror'));
+
+
+// according to tutorila
+function sarcasticCase2(str){
+    let newString = "";
+    for(let i=0; i < str.length; i++){
+        // if(i % 2 === 0 ){ // even number
+        //     newString = newString + str[i].toUpperCase();
+        // }else{
+        //     newString = newString + str[i].toLowerCase();
+        // }
+
+        newString = i % 2 ? newString + str[i].toUpperCase() : newString + str[i].toLowerCase();
+    }
+
+    return newString;
+}
+
+console.log(sarcasticCase2("hello man how are"));
